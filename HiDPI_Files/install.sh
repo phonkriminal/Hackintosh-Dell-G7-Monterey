278b6315eafd4553baa2bcd5731a6337c73771a7
Andrea Salvadori <phonkriminal@gmail.com>
2023-04-24 19:39:14 +0200

    HiDPI_Files
    Sound fix from Juan Castro

---
+++ HiDPI_Files/install.sh
@@ -0,0 +1,15 @@
+#!/bin/zsh
+
+echo "Enabling HiDPI..."
+
+sudo mkdir -p /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp Icons.plist /Library/Displays/Contents/Resources/Overrides/
+cd DisplayVendorID-30e4
+sudo cp DisplayProductID-5d0 /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp DisplayProductID-5d0.icns /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp DisplayProductID-540 /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+sudo cp DisplayProductID-540.icns /Library/Displays/Contents/Resources/Overrides/DisplayVendorID-30e4/
+
+echo "Done!"
+echo
+exit 1
